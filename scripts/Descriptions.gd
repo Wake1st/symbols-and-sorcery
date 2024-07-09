34482e59c4203e61c5e6494cb0fb0100a9ce6836
Wake1st <joeltwakefield@gmail.com>
2024-07-09 00:25:09 -0600

    WIP - functions setup; TODO: write

--- scripts/Descriptions.gd
+++ scripts/Descriptions.gd
@@ -10,12 +10,24 @@ static func load():
 		print("open error: ", FileAccess.get_open_error())
 	
 	descriptions = JSON.parse_string(json_as_text)
-	for room in descriptions["rooms"]:
-		rooms[room.name] = room
+	rooms = descriptions["rooms"]
 
 
-static func get_room_text(name:String) -> String:
-	var n = name.to_lower().replace("room","")
-	var room = rooms[name.to_lower().replace("room","")]
-	print("name: %s\t| room: %s" % [n, room])
-	return room["text"]
+static func get_room_text(room_name:String) -> String:
+	return rooms[room_name.to_lower().replace("room","")]["text"]
+
+
+static func get_token_text(room_name:String,token_name:String) -> String:
+	return rooms[
+		room_name.to_lower().replace("room","")
+	]["tokens"][
+		token_name.to_lower().replace("token","")
+	]
+
+
+static func get_interactable_text(room_name:String,interactable_name:String) -> String:
+	return rooms[
+		room_name.to_lower().replace("room","")
+	]["interactables"][
+		interactable_name.to_lower().replace("interactable","")
+	]
